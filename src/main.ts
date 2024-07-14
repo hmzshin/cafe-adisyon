ee7f6fdedfedf4dab9ff8603d49ef85a70313cb8
hamza sahin <hamza.sahin@hakanbt.com.tr>
2024-07-14 19:01:46 +0300

    feature: Login page  design is completed feature: add Iconify as icon pack fix: implement element plus refactor: rename package.json name

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import router from "./router/router";
+import ElementPlus from "element-plus";
 
 import App from "./App.vue";
+import "element-plus/dist/index.css";
 import "./index.css";
 
 const pinia = createPinia();
 const app = createApp(App);
 
+app.use(ElementPlus);
 app.use(pinia);
 app.use(router);
 app.mount("#app");
