fa2529ec43542211d2ad0694343eaa0fe0b507e3
Hamza Sahin <hamzasahin@outlook.com>
2024-12-22 17:21:47 +0300

    feature : create new color variable in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        'primary-color': '#026a52',
-        'header-bg-color': '#f8f9fa',
-        'body-bg-color': '#ffffff',
-        'danger-color': '#F56C6C',
+        "primary-color": "#026a52",
+        "add-btn": "#f8f9fa",
+        "header-bg-color": "#f8f9fa",
+        "body-bg-color": "#ffffff",
+        "danger-color": "#F56C6C",
       },
 
       backgroundImage: (theme) => ({
-        'hero-pattern': "url('/background.png')",
+        "hero-pattern": "url('/background.png')",
       }),
 
       fontFamily: {
-        sans: ['Monstretta', 'sans-serif'],
+        sans: ["Monstretta", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
+};
