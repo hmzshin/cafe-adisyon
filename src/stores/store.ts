81223de7b7fd3495b9af536aa964a1dc67cd76a5
Hamza Sahin <hamzasahin@outlook.com>
2024-12-22 17:26:27 +0300

    feature: add event listener to get screen size in pinia store

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,12 +1,27 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import { ref, computed, onMounted, onUnmounted } from "vue";
+import { defineStore } from "pinia";
 
-export const useCounterStore = defineStore('store', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+export const useStore = defineStore("store", () => {
+  const windowWidth = ref(window.innerWidth);
+  const windowHeight = ref(window.innerHeight);
+  const isMobile = computed(() => windowWidth.value < 640);
+  const isTablet = computed(
+    () => windowWidth.value >= 640 && windowWidth.value < 1024
+  );
+  const isDesktop = computed(() => windowWidth.value >= 1024);
+
+  function updateWindowSize() {
+    windowWidth.value = window.innerWidth;
+    windowHeight.value = window.innerHeight;
   }
 
-  return { count, doubleCount, increment }
-})
+  onMounted(() => {
+    window.addEventListener("resize", updateWindowSize);
+  });
+
+  onUnmounted(() => {
+    window.removeEventListener("resize", updateWindowSize);
+  });
+
+  return { isMobile, isTablet, isDesktop };
+});
